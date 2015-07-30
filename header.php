37977de1e3374ae822cb5bdff66361c18a4edc0d
John Tucker <john@larkintuckerllc.com>
2015-07-30 08:34:12 -0400

    Fixing for image alignment in rich editor.

--- header.php
+++ header.php
@@ -1,6 +1,9 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
   <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <title>Barebones</title>
     <?php wp_head() ?>
   </head>
   <body>
+    <div class="container">
--- page.php
+++ page.php
@@ -3,6 +3,7 @@
 <?php if (function_exists('yoast_breadcrumb')) {
    yoast_breadcrumb();
  }?>
+ <h1><?php the_title(); ?></h1>
 <?php while (have_posts()) : the_post(); ?>
   <?php the_content(); ?>
 <?php endwhile; ?>
